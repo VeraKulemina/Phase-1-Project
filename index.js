93c6aa6632f1296b247b2ef41bdb466e3d75ec02
VeraKulemina <verakulemina@gmail.com>
2022-10-05 10:14:13 -0600

    Vera commit

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
+const characterBar = document.getElementById("character-bar");
+const charactersImage = document.querySelector("#image");
+const cuteNames = document.querySelector("#name");
+const statusChar = document.querySelector("#vote-count");
+
 document.addEventListener("DOMContentLoaded", () => {
     fetch("https://rickandmortyapi.com/api/character/1,2,3,4,5")
       .then(response => response.json())
       .then(characters => {
-          characters.forEach(objCharacters => showCharacter(objCharacters))
-          displayCharacters(characters[0])
-        })})
+          characters.forEach(objCharacters => showCharacter(objCharacters));
+          //console.log(characters[0])
+          displayCharacters(characters[0]);
+        })});
 
-const characterBar = document.getElementById("character-bar");
-const charactersImage = document.querySelector("#image");
-const cuteNames = document.querySelector("#name");
-const statusChar = document.querySelector("#vote-count")
 characterBar.addEventListener("mouseover", (event) => {
-    // highlight the mouseover target
     event.target.style.color = "red";
 });
 characterBar.addEventListener("mouseout", (event) => {
-    // highlight the mouseover target
-    event.target.style.color = "#78BE21";
+        event.target.style.color = "#78BE21";
 });
 function showCharacter(objCharacter) {
     const img = document.createElement("span");
@@ -25,8 +25,7 @@ function showCharacter(objCharacter) {
     characterBar.append(img);
 }
 function displayCharacters(objCharacters) {
-    //console.log(characters)
-    cuteNames.textContent = objCharacters.name
+    cuteNames.textContent = objCharacters.name;
     statusChar.textContent = objCharacters.status;
    charactersImage.src = objCharacters.image
 }
